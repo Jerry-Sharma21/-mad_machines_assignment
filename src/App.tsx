d93236c86d40a1139c3e7e25dc516699d848de47
Abhijeet <abhijeet.vishu@gmail.com>
2024-08-23 21:53:00 +0530

    vite react app and basic cleanup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Offline Webhook App</h1>
+      <button className="button">Hit Me</button>
+    </div>
+  );
+};
+
+export default App;
